afd3c5947d1c08e4a330f72c1f1d36798462a8b7
Adonay <adonay.rssantos@gmail.com>
2025-04-30 12:47:06 -0300

    Including the user router in the app

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
 from contextlib import asynccontextmanager
-import os
-import signal
+import os, signal
+from database.db_config import create_tables
 from fastapi import FastAPI
+
+from routes.user_router import user_router
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     print("Initializing..")
+    create_tables()
     yield
     print("Finalizing")
     os.kill(os.getpid(), signal.SIGTERM)
     os.kill(os.getpid(), signal.SIGINT)
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(user_router)
 
 @app.get("/")
 def hello():
