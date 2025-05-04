1a90635993d2dce2ff67511f93e29598a70494bd
AdonaySantos <adonay.rssantos@gmail.com>
2025-05-04 00:20:57 -0300

    Updating the settings to follow the MongoDB structure

--- app/core/config_dotenv.py
+++ app/core/config_dotenv.py
@@ -3,4 +3,7 @@ from dotenv import load_dotenv
 
 _ = load_dotenv()
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set in the environment variables.")
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,58 +1,18 @@
 from typing import Annotated
 from fastapi import Depends
-from sqlalchemy.orm import Session
-from database.connection import get_db
-from models.projects import Projects
-from models.teams import Team
+from fastapi.responses import JSONResponse
+from pymongo.database import Database
+from database.mongo import get_database
 from schemas.user import UserSchema
-from models.users import User
 
 class UserService:
-    def user_register(self, json: list[UserSchema], sess: Annotated[Session, Depends(get_db)]):
+    def user_register(self, json: list[UserSchema], sess) -> JSONResponse:
         try:
-            teams_cache = {}
-
-            for user in json:
-                db_user = User(
-                    id=user.id,
-                    name=user.nome,
-                    age=user.idade,
-                    score=user.score,
-                    active=user.ativo,
-                    country=user.pais
-                )
-
-                team_name = user.equipe.nome
-                if team_name in teams_cache:
-                    db_team = teams_cache[team_name]
-                else:
-                    db_team = sess.query(Team).filter(Team.name == team_name).first()
-
-                    if not db_team:
-                        db_team = Team(
-                            name=user.equipe.nome,
-                            lider=user.equipe.lider,
-                            projects=[
-                                Projects(name=project.nome, completed=project.concluido) 
-                                for project in user.equipe.projetos
-                            ]
-                        )
-                        db.add(db_team)
-                
-                    teams_cache[team_name] = db_team  # salva no cache para próximos usuários
-
-                db_user.team = db_team
-
-                db_user.logs = [
-                    Log(date=log.data, action=log.acao)
-                    for log in user.logs
-                ] if user.logs else []
-
-                db.add(db_user)
-
-            db.commit()
-            return {"message": "Usuários inseridos com sucesso"}
-        
+            users_collection = sess["users"]
+            users_collection.insert_many(json)
+            print("deu bom")
+            return JSONResponse(status_code=200, content="deu bom")
         except Exception as e:
-            db.rollback()
-            return {"error": str(e)}
+            print("deu ruim")
+            return JSONResponse(status_code=500, content=f"deu ruim: {e}")
+
