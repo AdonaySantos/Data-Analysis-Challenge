e0a2d792553805794bd4ba49e8397745bdfe9fc5
AdonaySantos <adonay.rssantos@gmail.com>
2025-05-05 18:15:55 -0300

    GET /superusers endpoint created

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from contextlib import asynccontextmanager
 import os, signal
 
-from database.mongo import get_mongo
+from pymongo.cursor import Collection
+
+from database.mongo import get_user_collection
 from database.db_config import create_tables
 from fastapi import FastAPI
 
-from models.user import User
+from models.user import User, UserDict
+from schemas.user import list_users
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
@@ -20,10 +23,16 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/users")
 def post_users(users: list[User]):
-    db = get_mongo()
-    users_collection = db["users"]
+    users_collection = get_user_collection()
     _ = users_collection.insert_many(users)
     return {"message": f"{len(users)} users inserted into MongoDB"}
+
+@app.get("/superusers")
+def get_superusers() -> dict[str, list[UserDict] | int]:
+    collection: Collection[UserDict] = get_user_collection()
+    superusers_cursor = collection.find({"score": { "$gte": 900}, "ativo": True})
+    superusers = list_users(superusers_cursor)
+    return {"superusers": superusers, "number of superusers": len(superusers)}
 
 @app.get("/")
 def hello():
