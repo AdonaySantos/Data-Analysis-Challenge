e458be0bad0ff31ae4a0294913af85088d6b3174
AdonaySantos <adonay.rssantos@gmail.com>
2025-05-07 19:05:22 -0300

    Removed unused schemas

--- app/schemas/user.py
+++
@@ -1,9 +0,0 @@
-from pymongo.cursor import Cursor
-from models.user import UserDict
-
-def serialize_user(user: UserDict) -> UserDict:
-    user['_id'] = str(user['_id'])
-    return user
-
-def list_users(users: Cursor[UserDict]) -> list[UserDict]:
-    return [serialize_user(user) for user in users]
