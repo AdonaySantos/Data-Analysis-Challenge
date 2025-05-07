9b3787c015cf2b2b61004b00298dcdf7dfd264ff
AdonaySantos <adonay.rssantos@gmail.com>
2025-05-07 18:57:33 -0300

    Minor fixes

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -1,11 +1,12 @@
+from typing import Any
 from core.config_dotenv import DATABASE_URL
 from pymongo import MongoClient
-from models.user import User
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in environment variables.")
 
-client: MongoClient[User] = MongoClient(DATABASE_URL)
+client: MongoClient[Any] = MongoClient(DATABASE_URL)
 
-def get_mongo():
-    return client["json_users"]
+
+def get_user_collection():
+    return client["json_users"]["users"]
